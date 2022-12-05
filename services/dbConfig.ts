eab59102697bcde5e2a1b0bc1102b9cd6e1d590a
katcy <42054792+katcy@users.noreply.github.com>
2022-12-05 18:57:00 +0530

    Delete dbConfig.ts

--- services/dbConfig.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://djfsafgjzmpzvpgdmrsh.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqZnNhZmdqem1wenZwZ2RtcnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5ODQzMTgsImV4cCI6MTk4NTU2MDMxOH0.soEsTNYf2PGFziL3csbnpstNBk0BrzHDcaLLkdvDLTU";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
